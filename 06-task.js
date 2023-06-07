15304289d5df7135cf2ee885dc763e5f2a3ad4d9
RicardoOliveros <rica.oli0811@gmail.com>
2023-06-06 19:38:55 -0500

    segundo commit

---
+++ 06-task.js
@@ -0,0 +1,10 @@
+// Escribe un programa que con una variable número, muestre por consola si es mayor o menor que 10.
+
+let numero = 11;
+
+if (numero < 10){
+    console.log('MENOR')
+}
+else if (numero > 10){
+    console.log('MAYOR')
+}
---
+++ 07-task.js
@@ -0,0 +1,15 @@
+// Escribe un programa que con una variable de número, muestre por consola si es positivo, negativo o cero.
+
+let numero = 0;
+
+if (numero > 0){
+    console.log('POSITIVE')
+}
+
+else if(numero < 0){
+    console.log('NEGATIVE')
+}
+
+else if (numero == 0){
+    console.log('0')
+}
